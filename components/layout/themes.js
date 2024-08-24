85550476c98cd5038e777e7a6ef67bbedb3b1457
ritvikraj115 <143361673+ritvikraj115@users.noreply.github.com>
2024-08-25 02:09:21 +0530

    Add files via upload

---
+++ components/layout/themes.js
@@ -0,0 +1,22 @@
+const light = {
+    color: "#000",
+    bgColor: '#f0f0f0',
+    bgImage: 'linear-gradient(180deg, #f0f0f0 100%, #f0f0f0 40%)',
+    bgDiv: '#fff',
+    bgSubDiv: '#efe7fd'
+}
+
+const dark = {
+    color: '#fff',
+    bgColor: '#923cb5',
+    bgImage: 'linear-gradient(180deg, #2f0f3d 5%, #000000 90%)',
+    bgDiv: 'black',
+    bgSubDiv: 'rgb(33, 36, 41)'
+}
+
+const themes = {
+    light: light,
+    dark: dark,
+}
+
+export default themes;
